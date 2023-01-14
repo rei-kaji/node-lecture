e9f3c3f992655234a737678e3bad26e0cbf29e8a
rei_kaji <st.r.z.rei@gmail.com>
2023-01-13 16:55:55 -0800

    add paging function

--- node-begin/server-express.js
+++ node-begin/server-express.js
@@ -18,11 +18,21 @@ app.get("/home", function (req, res) {
   res.sendFile(__dirname + "/view" + "/index.html");
 });
 
+app.get("/about", function (req, res) {
+  res.sendFile(__dirname + "/view" + "/about.html");
+});
+
+app.get("/contact", function (req, res) {
+  res.sendFile(__dirname + "/view" + "/contact.html");
+});
+
+app.get("/landing", function (req, res) {
+  res.sendFile(__dirname + "/view" + "/landing.html");
+});
+
 app.get("/person", function (req, res) {
   res.setHeader("Content-Type", "text/json");
   res.json({ name: "Rei", height: "170cm" });
 });
 
 app.listen(5001);
-
-// contactページとlandingページを作る
